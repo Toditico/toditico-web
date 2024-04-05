7189af893b38bb5e2c777916526ad270641b33b3
Jose Gabriel Companioni Benitez <gcompanionib@gmail.com>
2024-04-05 17:42:29 -0400

    Finished Hero Section

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,11 +7,21 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    fontSize: {
+      h1: ["28px", "37.8px"],
+      h3: ["22px", "29.7px"],
+      button: ["14px", "18.9px"],
+    },
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      colors: {
+        primary: "#CE2C0D",
+        secondary: "#DD562C",
+        tertiary: "#041A42",
       },
     },
   },
