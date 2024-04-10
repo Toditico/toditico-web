a0e7e8c12b5d5ad9e01af36edd9ef15c7bee49cb
Jose Gabriel Companioni Benitez <gcompanionib@gmail.com>
2024-04-10 12:32:23 -0400

    Updated footer

--- src/components/layout/ContactUs.tsx
+++ src/components/layout/ContactUs.tsx
@@ -8,8 +8,8 @@ export default function ContactUs() {
   const time = "Todos los días de 9:00 am - 5:00 pm";
   return (
     <div className="bg-gray py-12 px-6 flex flex-col gap-[10px] items-center xl:flex-row xl:px-12 xl:justify-between">
-      <Image src={brand} alt="Toditico Logo" />
-      <div className="flex flex-col gap-4 text-center">
+      <Image src={brand} alt="Toditico Logo" className="xl:w-[240px]" />
+      <div className="flex flex-col gap-4 text-center xl:text-left">
         <p className="text-body-bold font-bold uppercase">contáctenos</p>
         <div className="flex flex-col gap-2">
           <p className="text-body">{email}</p>
@@ -17,6 +17,10 @@ export default function ContactUs() {
           <p className="text-body">{phones}</p>
           <p className="text-body">{time}</p>
         </div>
+      </div>
+      <div className="flex flex-col gap-4 text-center xl:text-left xl:max-w-[300px]">
+        <p className="text-body-bold font-bold uppercase">Rueda con confianza</p>
+        <p className="text-body">Servicio superior, precios justos, calidad insuperable y garantía que perdura.</p>
       </div>
     </div>
   );
