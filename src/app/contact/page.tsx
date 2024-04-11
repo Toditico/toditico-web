4a98a26897c815c4cf355884d64c1c3cde362aa1
Jose Gabriel Companioni Benitez <gcompanionib@gmail.com>
2024-04-11 11:07:27 -0400

    Finished mobile breakpoint about us view

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,10 @@
+"use client";
+import BussinessInfo from "@/components/contact/BussinessInfo";
+
+export default function Contact() {
+  return (
+    <div className="flex flex-col items-center px-6 py-12">
+      <BussinessInfo />
+    </div>
+  );
+}
---
+++ src/components/contact/BussinessInfo.tsx
@@ -0,0 +1,52 @@
+export default function BussinessInfo() {
+  return (
+    <div className="flex flex-col gap-6">
+      <div className="flex flex-col gap-4 text-center items-center mb-8">
+        <p className="text-h2-tablet text-primary font-bold">Que es Toditico</p>
+        <div className="w-[160px] h-[4px] rounded bg-primary"></div>
+      </div>
+      <p className="text-button text-center">
+        TODITICO es un proyecto relativamente joven, con el concepto de tienda
+        comercializadora de partes y piezas de la industria automotriz en el
+        mercado cubano.
+        <br />
+        Surge en enero del 2022, como resultado del deseo profundo de satisfacer
+        las necesidades en dicho mercado, apostando siempre al crecimiento y
+        profesionalidad. Abarcando diferentes nichos sólidamente, hemos logrado
+        convertirnos en una gran oportunidad para nuestros clientes, ofreciendo
+        soluciones confiables y duraderas para mantener los vehículos en su
+        mejor estado y garantizar la seguridad en las carreteras.
+      </p>
+      <p className="text-button text-center">
+        Con la vista en nuestra prioridad, la satisfacción del cliente, contamos
+        con un equipo altamente capacitado y apasionado por la industria,
+        siempre dispuesto a dar el soporte necesario a nuestros clientes,
+        proporcionando un servicio amable y eficiente.
+      </p>
+      <div className="flex flex-col gap-4 text-center">
+        <p className="font-bold text-button">
+          Misión: <br />
+          <span className="font-normal text-button">
+            Proveer partes y piezas de la industria automotriz, en el mercado
+            cubano.
+          </span>
+        </p>
+        <p className="font-bold text-button">
+          Visión: <br />
+          <span className="font-normal text-button">
+            En función de nuestra misión, ser el proyecto más exitoso y
+            respetado en Cuba. Con un stock estable, de fácil alcance y precios
+            altamente competitivos. Ofrecer un servicio de venta y postventa de
+            excelencia. cubano.
+          </span>
+        </p>
+        <p className="font-bold text-button">
+          Valores: <br />
+          <span className="font-normal text-button">
+            Integridad, transparencia, compromiso y enfoque.
+          </span>
+        </p>
+      </div>
+    </div>
+  );
+}
