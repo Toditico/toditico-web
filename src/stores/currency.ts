be01045746295b0428d60bfc4b4eaa8cd25e65b8
Jose Gabriel Companioni Benitez <gcompanionib@gmail.com>
2024-05-14 13:42:14 -0400

    Using local storage to maintain selected currency and inventory

--- src/stores/currency.ts
+++ src/stores/currency.ts
@@ -3,20 +3,30 @@ import { create } from "zustand";
 
 type CurrencyState = {
   currencies: Currency[];
-  selectedCurrency?: Currency;
+  selectedCurrency: Currency | null;
   setSelectedCurrency: (currency: Currency) => void;
   setCurrencies: (currencies: Currency[]) => void;
 };
 
+const initialSelectedCurrency = (): Currency | null => {
+  const storageCurrency = localStorage.getItem("currency");
+  return storageCurrency ? JSON.parse(storageCurrency) : null;
+};
+
 export const useCurrencyStore = create<CurrencyState>((set) => ({
   currencies: [],
+  selectedCurrency: initialSelectedCurrency(),
   setSelectedCurrency: (currency) =>
     set(() => {
+      localStorage.setItem("currency", JSON.stringify(currency));
       return { selectedCurrency: currency };
     }),
   setCurrencies: (currencies) =>
     set((state) => {
       const { selectedCurrency } = state;
+      console.log("selected currency: ", selectedCurrency);
+      !selectedCurrency &&
+        localStorage.setItem("currency", JSON.stringify(currencies[0]));
       return {
         currencies,
         selectedCurrency: selectedCurrency ?? currencies[0],
--- src/stores/inventory.ts
+++ src/stores/inventory.ts
@@ -3,17 +3,24 @@ import { create } from "zustand";
 
 type InventoryState = {
   inventories: Inventory[];
-  selectedInventory?: Inventory;
+  selectedInventory: Inventory | null;
   setSelectedInventory: (inventory: Inventory) => void;
   setInventories: (inventories: Inventory[]) => void;
   openSelectionModal: boolean;
   setOpenSelectionModal: (open: boolean) => void;
 };
 
+const initialSelectedInventory = (): Inventory | null => {
+  const storageInventory = localStorage.getItem("inventory");
+  return storageInventory ? JSON.parse(storageInventory) : null;
+};
+
 export const useInventoryStore = create<InventoryState>((set) => ({
   inventories: [],
+  selectedInventory: initialSelectedInventory(),
   setSelectedInventory: (inventory) =>
     set(() => {
+      localStorage.setItem("inventory", JSON.stringify(inventory));
       return { selectedInventory: inventory };
     }),
   setInventories: (inventories) =>
