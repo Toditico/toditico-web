7429cedc1dbc4fa21e21ba7dd325159af78f6115
Jose Gabriel Companioni Benitez <gcompanionib@gmail.com>
2024-09-19 00:42:49 -0400

    Adding functionality to show image details in a modal

---
+++ src/components/layout/NextImageSlide/index.tsx
@@ -0,0 +1,70 @@
+import Image from "next/image";
+import {
+  ContainerRect,
+  Slide,
+  isImageFitCover,
+  isImageSlide,
+  useLightboxProps,
+  useLightboxState,
+} from "yet-another-react-lightbox";
+
+function isNextJsImage(slide: Slide) {
+  return (
+    isImageSlide(slide) &&
+    typeof slide.width === "number" &&
+    typeof slide.height === "number"
+  );
+}
+
+type Props = {
+  slide: Slide;
+  offset: number;
+  rect: ContainerRect;
+};
+
+export default function NextImageSlide({ slide, offset, rect }: Props) {
+  const {
+    on: { click },
+    carousel: { imageFit },
+  } = useLightboxProps();
+
+  const { currentIndex } = useLightboxState();
+
+  const cover = isImageSlide(slide) && isImageFitCover(slide, imageFit);
+
+  if (!isNextJsImage(slide)) return undefined;
+
+  console.log("Width and height: ", slide.width, slide.height);
+
+  const width = !cover
+    ? Math.round(
+        Math.min(rect.width, (rect.height / slide.height!) * slide.width!),
+      )
+    : rect.width;
+
+  const height = !cover
+    ? Math.round(
+        Math.min(rect.height, (rect.width / slide.width!) * slide.height!),
+      )
+    : rect.height;
+
+  return (
+    <div style={{ position: "relative", width, height }}>
+      <Image
+        fill
+        alt=""
+        src={slide.src}
+        loading="eager"
+        draggable={false}
+        style={{
+          objectFit: cover ? "cover" : "contain",
+          cursor: click ? "pointer" : undefined,
+        }}
+        sizes={`${Math.ceil((width / window.innerWidth) * 100)}vw`}
+        onClick={
+          offset === 0 ? () => click?.({ index: currentIndex }) : undefined
+        }
+      />
+    </div>
+  );
+}
--- src/components/product/ProductDetails/ProductImages.tsx
+++ src/components/product/ProductDetails/ProductImages.tsx
@@ -2,6 +2,10 @@
 import Image, { StaticImageData } from "next/image";
 import { useEffect, useState } from "react";
 import Placeholder from "@public/images/placeholder.webp";
+import Lightbox from "yet-another-react-lightbox";
+import Zoom from "yet-another-react-lightbox/plugins/zoom";
+import "yet-another-react-lightbox/styles.css";
+import NextImageSlide from "@/components/layout/NextImageSlide";
 
 type Props = {
   images: string[];
@@ -11,6 +15,7 @@ export default function ProductImages({ images }: Props) {
   const [selectedImageUrl, setSelectedImageUrl] = useState<
     string | StaticImageData
   >(Placeholder);
+  const [openModal, setOpenModal] = useState(false);
 
   useEffect(() => {
     if (typeof selectedImageUrl === "string") {
@@ -19,16 +24,37 @@ export default function ProductImages({ images }: Props) {
     images.length && setSelectedImageUrl(images[0]);
   }, [images]);
 
+  const deviceSizes = [640, 750, 828, 1080, 1200, 1920, 2048, 3840];
+
+  function nextImageUrl(src: string) {
+    const width = deviceSizes.find(
+      (deviceSize) => deviceSize > window.innerWidth,
+    );
+    return `/_next/image?url=${encodeURIComponent(src)}&w=${width}&q=100`;
+  }
   return (
     <div className="flex flex-col gap-2">
       <div className="w-full h-[320px] relative">
         <Image
+          onClick={() => setOpenModal(true)}
           src={selectedImageUrl}
           alt="Product image"
           fill
           className="rounded-lg"
           style={{ objectFit: "contain" }}
           quality={100}
+        />
+        <Lightbox
+          open={openModal}
+          close={() => setOpenModal(false)}
+          render={{ slide: NextImageSlide }}
+          slides={[
+            typeof selectedImageUrl === "string"
+              ? { src: nextImageUrl(selectedImageUrl) }
+              : selectedImageUrl,
+          ]}
+          plugins={[Zoom]}
+	  zoom={{maxZoomPixelRatio: 3}}
         />
       </div>
     </div>
