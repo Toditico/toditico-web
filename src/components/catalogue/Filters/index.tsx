11db41037521c73ce60ed470f483b64a5574629b
Jose Gabriel Companioni Benitez <gcompanionib@gmail.com>
2024-11-21 11:47:00 -0500

    Improved Filters selection component in Tablet breakpoint

--- src/components/catalogue/Filters/index.tsx
+++ src/components/catalogue/Filters/index.tsx
@@ -19,7 +19,6 @@ export default function Filters({
   selectedInventory,
   selectedQuery,
   onFilter,
-  isLoading = false,
 }: Props) {
   const [inventory, setInventory] = useState<string>("");
   const [productName, setProductName] = useState<string>("");
@@ -36,76 +35,70 @@ export default function Filters({
 
   return (
     <div
-      className="flex flex-col p-6 gap-4"
+      className="flex flex-col p-6 gap-4 md:flex-row"
       onKeyDown={(e) => {
         if (e.code === "Enter") {
           onFilter(productName, inventory);
         }
       }}
     >
-      {isLoading ? (
-        <>
-          <Skeleton variant="rectangular" height={56} width={"100%"} />
-          <Skeleton variant="rectangular" height={56} width={"100%"} />
-          <Skeleton variant="rectangular" height={56} width={"100%"} />
-        </>
-      ) : (
-        <>
-          <Select
-            placeholder="Inventario"
-            value={inventory}
-            onChange={(el) => setInventory(el.target.value)}
-            sx={{
-              height: "56px",
-              borderRadius: "8px",
-              fontSize: "16px",
-              lineHeight: "21.6px",
-              borderWidth: "2px",
-              borderColor: colors.primary,
-              "&.Mui-focused": {
-                border: "none",
-              },
-            }}
-          >
-            {inventories.map((inventory) => (
-              <MenuItem key={inventory.name} value={inventory._id}>
-                {inventory.name}
-              </MenuItem>
-            ))}
-          </Select>
+      <>
+        <Select
+          className="flex-grow"
+          placeholder="Inventario"
+          value={inventory}
+          onChange={(el) => setInventory(el.target.value)}
+          sx={{
+            height: "56px",
+            borderRadius: "8px",
+            fontSize: "16px",
+            lineHeight: "21.6px",
+            borderWidth: "2px",
+            borderColor: colors.primary,
+            "&.Mui-focused": {
+              border: "none",
+            },
+          }}
+        >
+          {inventories.map((inventory) => (
+            <MenuItem key={inventory.name} value={inventory._id}>
+              {inventory.name}
+            </MenuItem>
+          ))}
+        </Select>
 
-          <TextField
-            placeholder="Nombre"
-            type="search"
-            value={productName}
-            onChange={(event) => setProductName(event.target.value)}
-            InputProps={{
-              ...{
-                sx: {
-                  height: "56px",
-                  borderRadius: "8px",
-                  fontSize: "16px",
-                  lineHeight: "21.6px",
-                  borderWidth: "2px",
-                  borderColor: colors.primary,
-                  "&.Mui-focused": {
-                    border: "none",
-                  },
+        <TextField
+          className="flex-grow"
+          placeholder="Nombre"
+          type="search"
+          value={productName}
+          onChange={(event) => setProductName(event.target.value)}
+          InputProps={{
+            ...{
+              sx: {
+                height: "56px",
+                borderRadius: "8px",
+                fontSize: "16px",
+                lineHeight: "21.6px",
+                borderWidth: "2px",
+                borderColor: colors.primary,
+                "&.Mui-focused": {
+                  border: "none",
                 },
               },
-            }}
-          />
+            },
+          }}
+        />
 
-          <Button
-            className="h-[56px] rounded-lg p-4 uppercase font-bold text-button"
-            variant="contained"
-            startIcon={<IconFilter size={24} />}
-            onClick={() => onFilter(productName, inventory)}
-          >
-            Filtrar
-          </Button>
-        </>
-      )}
+        <Button
+          className="h-[56px] rounded-lg p-4 uppercase font-bold text-button"
+          variant="contained"
+          startIcon={<IconFilter size={24} />}
+          onClick={() => onFilter(productName, inventory)}
+        >
+          Filtrar
+        </Button>
+      </>
     </div>
   );
 }
