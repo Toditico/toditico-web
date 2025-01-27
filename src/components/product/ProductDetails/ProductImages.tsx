d060e5e0d99b3e5633301a6b15ca1f2488852515
Jose Gabriel Companioni Benitez <gcompanionib@gmail.com>
2025-01-27 23:34:46 +0400

    Finished behavior for products with secondary images

--- src/components/product/ProductDetails/ProductImages.tsx
+++ src/components/product/ProductDetails/ProductImages.tsx
@@ -19,6 +19,11 @@ export default function ProductImages({ images }: Props) {
 
   const secondaryImages = images.slice(1);
 
+  const openImagesModal = (index: number) => {
+    setIndex(index);
+    openModal();
+  };
+
   useEffect(() => {
     if (typeof selectedImageUrl === "string") {
       return;
@@ -26,21 +31,22 @@ export default function ProductImages({ images }: Props) {
     images.length && setSelectedImageUrl(images[0]);
   }, [images]);
 
-  const openImagesModal = () => {
-    setSlides([
-      typeof selectedImageUrl === "string"
-        ? { src: nextImageUrl(selectedImageUrl) }
-        : selectedImageUrl,
-    ]);
-    openModal();
-  };
+  useEffect(() => {
+    setSlides(
+      images.map((imageData) =>
+        typeof imageData === "string"
+          ? { src: nextImageUrl(imageData) }
+          : imageData,
+      ),
+    );
+  }, [images]);
 
   return (
     <div className="flex flex-col gap-2 xl:w-[500px]">
       <div className="w-full h-[320px] relative xl:h-[340px] xl:w-[412px]">
         <Image
           onClick={() => {
-            openImagesModal();
+            openImagesModal(0);
           }}
           src={selectedImageUrl}
           alt="Product image"
@@ -52,12 +58,16 @@ export default function ProductImages({ images }: Props) {
       </div>
       <div className="flex gap-6 flex-wrap">
         {secondaryImages.map((secondaryImage, index) => (
-          <div key={secondaryImage} className="w-20 h-20 relative bg-white shadow-cart-images">
+          <div
+            key={secondaryImage}
+            className="w-20 h-20 relative bg-white shadow-cart-images"
+          >
             <Image
               src={secondaryImage}
               alt="Product secondary image"
               fill
               style={{ objectFit: "contain" }}
+              onClick={() => openImagesModal(index + 1)}
             />
           </div>
         ))}
