f2b545923953eb34553808e730aacb113426b5eb
Jose Gabriel Companioni Benitez <gcompanionib@gmail.com>
2025-03-03 14:38:16 +0400

    Updated social networks component

--- src/components/layout/SocialNetworks.tsx
+++ src/components/layout/SocialNetworks.tsx
@@ -1,9 +1,11 @@
 import instagram from "../../../public/images/instagram.svg";
 import linkedin from "../../../public/images/linkedin.svg";
 import facebook from "../../../public/images/facebook.svg";
+import youtube from "../../../public/images/youtube.svg";
 import instagramAlt from "../../../public/images/instagram-alt.svg";
 import linkedinAlt from "../../../public/images/linkedin-alt.svg";
 import facebookAlt from "../../../public/images/facebook-alt.svg";
+import youtubeAlt from "../../../public/images/youtube-alt.svg";
 import Image from "next/image";
 
 type Props = {
@@ -11,20 +13,36 @@ type Props = {
 };
 
 export default function SocialNetworks({ isAlternative = false }: Props) {
+  const facebookLink = "https://www.facebook.com/share/15hopRCBA3/";
+  const instagramLink =
+    "https://www.instagram.com/toditico_cuba?igsh=MWxzNHJpZHBycjB6Nw==";
+  const linkedinLink = "https://www.linkedin.com/company/toditico/";
+  const youtubeLink = "https://youtube.com/@toditico_cuba?feature=shared";
+
   const links = isAlternative
-    ? [{ src: facebookAlt }, { src: instagramAlt }, { src: linkedinAlt }]
-    : [{ src: facebook }, { src: instagram }, { src: linkedin }];
+    ? [
+        { src: facebookAlt, link: facebookLink },
+        { src: instagramAlt, link: instagramLink },
+        { src: linkedinAlt, link: linkedinLink },
+        {
+          src: youtubeAlt,
+          link: youtubeLink,
+        },
+      ]
+    : [
+        { src: facebook, link: facebookLink },
+        { src: instagram, link: instagramLink },
+        { src: linkedin, link: linkedinLink },
+        { src: youtube, link: youtubeLink },
+      ];
 
   return (
     <>
       <div className="flex flex-row gap-2">
-        {links.map(({ src }) => (
-          <Image
-            {...{ src }}
-            height={24}
-            alt="social"
-            key={JSON.stringify(src)}
-          />
+        {links.map(({ src, link }) => (
+          <a href={link} key={link} target="_blank">
+            <Image {...{ src }} height={24} alt="social" />
+          </a>
         ))}
       </div>
     </>
