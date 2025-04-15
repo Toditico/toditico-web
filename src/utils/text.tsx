cdc8d9ce9323183e1187d75013fb78cbf65bdb67
Jose Gabriel Companioni Benitez <gcompanionib@gmail.com>
2025-04-15 23:39:51 +0400

    Solving issue related to the link wrap

--- src/utils/text.tsx
+++ src/utils/text.tsx
@@ -5,7 +5,7 @@ export const renderTextWithLinks = (
   const urlRegex = /(https?:\/\/[^\s]+)/g;
 
   const descriptionWithLinks = description.replace(urlRegex, (url) => {
-    return `<a href="${url}" target="_blank" rel="noopener noreferrer" class="underline" onclick="event.stopPropagation();" style="color: #3b82f6">${url}</a>`;
+    return `<a href="${url}" target="_blank" rel="noopener noreferrer" class="underline wrap-anywhere" onclick="event.stopPropagation();" style="color: #3b82f6; overflow-wrap: anywhere;">${url}</a>`;
   });
 
   return (
