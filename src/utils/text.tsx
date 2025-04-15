f412ad8ca293ee9f6b61a80dfb01fc7d78003c66
Jose Gabriel Companioni Benitez <gcompanionib@gmail.com>
2025-04-15 23:27:56 +0400

    Making links in the products description to render blue

--- src/utils/text.tsx
+++ src/utils/text.tsx
@@ -1,14 +1,17 @@
-  export const renderTextWithLinks = (description: string, generalClasses: string) => {
-    const urlRegex = /(https?:\/\/[^\s]+)/g;
+export const renderTextWithLinks = (
+  description: string,
+  generalClasses: string,
+) => {
+  const urlRegex = /(https?:\/\/[^\s]+)/g;
 
-    const descriptionWithLinks = description.replace(urlRegex, (url) => {
-      return `<a href="${url}" target="_blank" rel="noopener noreferrer" class="text-blue-500 underline" onclick="event.stopPropagation();">${url}</a>`;
-    });
+  const descriptionWithLinks = description.replace(urlRegex, (url) => {
+    return `<a href="${url}" target="_blank" rel="noopener noreferrer" class="underline" onclick="event.stopPropagation();" style="color: #3b82f6">${url}</a>`;
+  });
 
-    return (
-      <p
-        className={generalClasses}
-        dangerouslySetInnerHTML={{ __html: descriptionWithLinks }}
-      />
-    );
-  };
+  return (
+    <p
+      className={generalClasses}
+      dangerouslySetInnerHTML={{ __html: descriptionWithLinks }}
+    />
+  );
+};
