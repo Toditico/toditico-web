009327f127cfa767dfb824e0fcfc9285328554b5
Jose Gabriel Companioni Benitez <53531665+elC0mpa@users.noreply.github.com>
2025-11-21 18:39:03 +0400

    Disabled cache

--- src/actions/productActions.ts
+++ src/actions/productActions.ts
@@ -9,65 +9,49 @@ const GET_PRODUCT_DETAILS_REVALIDATE_SECONDS = 90;
 const FILTER_PRODUCTS_REVALIDATE_SECONDS = 90;
 const AUTOCOMPLETE_PRODUCTS_REVALIDATE_SECONDS = 60;
 
-const getProductsDetailsAction = unstable_cache(
-  async (
-    codes: string[],
-    inventory: string,
-    currency: string,
-  ): Promise<Product[]> => {
-    const products = await productService.getDetails(
-      codes,
-      inventory,
-      currency,
-    );
-    return normalizeProductsData(products);
-  },
-  undefined,
-  { revalidate: false },
-);
+const getProductsDetailsAction = async (
+  codes: string[],
+  inventory: string,
+  currency: string,
+): Promise<Product[]> => {
+  const products = await productService.getDetails(codes, inventory, currency);
+  return normalizeProductsData(products);
+};
 
-const filterProductsAction = unstable_cache(
-  async (
-    text: string,
-    inventoryId: string,
-    currencyId: string,
-    moduleId: string,
-    page: number,
-    limit: number,
-  ): Promise<FilterProductsType> => {
-    const { result, paginationInfo } = await productService.filterProducts(
-      text.trim(),
-      inventoryId,
-      currencyId,
-      moduleId,
-      page,
-      limit,
-    );
-    return {
-      result: normalizeProductsData(result),
-      paginationInfo,
-    };
-  },
-  undefined,
-  { revalidate: FILTER_PRODUCTS_REVALIDATE_SECONDS },
-);
+const filterProductsAction = async (
+  text: string,
+  inventoryId: string,
+  currencyId: string,
+  moduleId: string,
+  page: number,
+  limit: number,
+): Promise<FilterProductsType> => {
+  const { result, paginationInfo } = await productService.filterProducts(
+    text.trim(),
+    inventoryId,
+    currencyId,
+    moduleId,
+    page,
+    limit,
+  );
+  return {
+    result: normalizeProductsData(result),
+    paginationInfo,
+  };
+};
 
-const getProductsAutocompleteAction = unstable_cache(
-  async (
-    text: string,
-    inventoryId: string,
-    currencyId: string,
-  ): Promise<Product[]> => {
-    const data = await productService.getAutocomplete(
-      text.trim(),
-      inventoryId,
-      currencyId,
-    );
-    return normalizeProductsData(data);
-  },
-  undefined,
-  { revalidate: AUTOCOMPLETE_PRODUCTS_REVALIDATE_SECONDS },
-);
+const getProductsAutocompleteAction = async (
+  text: string,
+  inventoryId: string,
+  currencyId: string,
+): Promise<Product[]> => {
+  const data = await productService.getAutocomplete(
+    text.trim(),
+    inventoryId,
+    currencyId,
+  );
+  return normalizeProductsData(data);
+};
 
 export {
   getProductsDetailsAction,
